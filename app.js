cf5f3955445787c6a2f9a7248cc053050b8c2211
sabbir1020 <gazisabbir924@gmail.com>
2021-08-21 21:28:41 +0600

    style.css add

--- app.js
+++ app.js
@@ -12,7 +12,8 @@
   const bestPriceText = bestPrice.innerText;
   const totalAmount = document.getElementById('total-amount');
   const totalAmountText = totalAmount.innerText;
-
+   
+  // updateTotalPrice 
     function updateToralPrice (){
         const bestTotal = Number(bestPrice.innerText);
         const memoryPuduct = Number(extramMemory.innerText);
@@ -24,71 +25,55 @@
         
     }
 
-
-
 document.getElementById('memory-6GB').addEventListener('click',function(){
-    
-    // const extramMemory = document.getElementById('extra-memory');
-    // const extramMemoryText = extramMemory.innerText;
+      
     extramMemory.innerText = '0';
     updateToralPrice();
-
-    // chageing total 
-    // totalPrice.innerText = 1200;
    
-    // console.log(extramMemoryText);
-   
-})
+ })
 document.getElementById('memory-16GB').addEventListener('click',function(){
 
-    // const extramMemory = document.getElementById('extra-memory');
-    // const extramMemoryText = extramMemory.innerText;
     extramMemory.innerText = '180';
     updateToralPrice();
-    // console.log(extramMemoryText);
+    
 })
 // storage SSD 
 document.getElementById('storage-256GB').addEventListener('click',function(){
-    // const extraStorege = document.getElementById('extra-storege');
-    // const extraStoregeText = extraStorege.innerText;
+    
     extraStorege.innerText = 0;
     updateToralPrice();
-    // console.log('click storage 256');
+   
     
 })
 document.getElementById('storage-512GB').addEventListener('click',function(){
-    // const extraStorege = document.getElementById('extra-storege');
-    // const extraStoregeText = extraStorege.innerText;
+    
     extraStorege.innerText = 150;
     updateToralPrice();
-    // console.log('click storage 512');
+   
 })
 document.getElementById('storage-1BT').addEventListener('click',function(){
-    // const extraStorege = document.getElementById('extra-storege');
-    // const extraStoregeText = extraStorege.innerText;
+   
     extraStorege.innerText = 200 ;
     updateToralPrice();
-    // console.log('click storage 1TB');
+   
 })
 // delivary charges 
 document.getElementById('delivery-free').addEventListener('click',function(){
-    // const deliveryPrice =document.getElementById('delivery-price');
-    // const deliveryPriceText = deliveryPrice.innerText;
+    
     deliveryPrice.innerText = 0;
     updateToralPrice();
-    // console.log(deliveryPrice);
+  
 })
 document.getElementById('delivery-charge').addEventListener('click',function(){
-    // const deliveryPrice =document.getElementById('delivery-price');
-    // const deliveryPriceText = deliveryPrice.innerText;
+   
     deliveryPrice.innerText = 20;
     updateToralPrice();
-    // console.log('delivery charge');
+    
 })
-// total price 
-// document.getElementById('best-price');  
-// const totalPrice = document.getElementById('total-price');
-// totalPriceText  = totalPrice.innerText; 
-// function updateToralPrice (){
-//     console.log(totalPrice.innerText,extramMemory.innerText);
-// }
+
+// promo code use 
+document.getElementById('apply-button').addEventListener('click',function(){
+    const promoInput = document.getElementById('promo-input');
+    const promoInputText = promoInput.value;
+   
+}) 
